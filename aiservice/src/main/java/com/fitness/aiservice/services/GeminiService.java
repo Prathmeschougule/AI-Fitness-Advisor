806c0ae2944e0fb822d937bf220b52990032f5b7
Prathmesh Chougule <prathmeshuix@gmail.com>
2025-05-12 23:32:33 +0530

    Integration Gemini Api

---
+++ aiservice/src/main/java/com/fitness/aiservice/services/GeminiService.java
@@ -0,0 +1,30 @@
+package com.fitness.aiservice.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+
+@Service
+public class GeminiService {
+
+    private  final WebClient webClient;
+
+    public GeminiService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String getAnswer(String question) {
+        Map<String,Object> requestBody= Map.of(
+                "contents" , new Object[]{
+                        Map.of(
+                                "parts" , new Object[]{
+                                       Map.of("text" , question)
+                                }
+                        )
+                }
+        );
+    }
+
+}
