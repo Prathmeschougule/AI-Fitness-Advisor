fcf691824272b1d28e73f087dbda28051d2fffe5
Prathmesh Chougule <prathmeshuix@gmail.com>
2025-05-14 23:32:30 +0530

    Ai-Services Making the Dto Ai-Service api Check Create ActivityAiService Work On ActivityAiService

--- aiservice/src/main/java/com/fitness/aiservice/services/ActivityAiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/services/ActivityAiService.java
@@ -1,8 +1,64 @@
 package com.fitness.aiservice.services;
 
 
+import com.fitness.aiservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ActivityAiService {
+
+    private GeminiService geminiService;
+
+    public  String generateRecommendation(Activity activity){
+
+        String prompt = createPromptForActivity(activity);
+        String aiResponse = geminiService.getAnswer(prompt);
+        log.info("RESPONSE FROM THE AI : {} " , aiResponse);
+        return  aiResponse;
+    }
+
+    private String createPromptForActivity(Activity activity) {
+        return String.format(""" 
+                  Analyze this fitness activity and provide detailed recommendations in the following format
+                  {
+                      "analysis" : {
+                          "overall": "Overall analysis here",
+                          "pace": "Pace analysis here",
+                          "heartRate": "Heart rate analysis here",
+                          "CaloriesBurned": "Calories Burned here"
+                      },
+                      "improvements": [
+                          {
+                              "area": "Area name",
+                              "recommendation": "Detailed Recommendation"
+                          }
+                      ],
+                      "suggestions" : [
+                          {
+                              "workout": "Workout name",
+                              "description": "Detailed workout description"
+                          }
+                      ],
+                      "safety": [
+                          "Safety point 1",
+                          "Safety point 2"
+                      ]
+                  }
+                
+                  Analyze this activity:
+                  Activity Type: %s
+                  Duration: %d minutes
+                  calories Burned: %d
+                  Additional Metrics: %s
+                
+                  provide detailed analysis focusing on performance, improvements, next workout suggestions, and safety guidelines
+                  Ensure the response follows the EXACT JSON format shown above.
+                """
+        );
+    }
+
 }
